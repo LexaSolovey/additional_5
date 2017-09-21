26af4ac7d1a15392a57029c43e5cf35df2953f89
LexaSolovey <xoxach5@gmail.com>
2017-09-21 06:37:01 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = {},
+        closeBrackets = {},
+        sameBrackets = [],
+        stack = [],
+        splitStr = str.split('');
+  let result = true;
+  bracketsConfig.forEach((item) => {
+    if (item[0] === item[1]) sameBrackets.push(item[0]);
+    else {
+      openBrackets[item[0]] = item[1];
+      closeBrackets[item[1]] = item[0];
+    }
+  });
+  splitStr.forEach((item) => {
+    if (openBrackets[item]){
+      stack.push(item);
+      return;
+    }
+    if (closeBrackets[item]){
+      if(stack.pop() !== closeBrackets[item]) result = false;
+      return;
+    }
+    if (sameBrackets.indexOf(item) > -1) stack.slice(-1).pop() === item ? stack.pop() : stack.push(item);
+  });
+  if (stack.length > 0) result = false;
+  return result;
 }
